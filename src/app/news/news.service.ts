284a3b170d6681fc46361b5df733c257cc9e0c7b
Rohit Bandooni <rbandooni@live.in>
2018-10-22 12:24:18 -0400

    added news service

---
+++ src/app/news/news.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { News } from './news';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+
+  private key = 'AIzaSyClh6k8f3qzNPxr4cTdheHyFfL0HNdzESg';
+  constructor(private http: HttpClient) { }
+
+  getAllPosts(): Observable<any> {
+    
+    return this.http.get(`https://www.googleapis.com/blogger/v3/blogs/5022270470070241638/posts?maxResults=14&key=${this.key}`)
+  }
+}
